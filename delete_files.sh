02b8530ee84c3434c8c2335c672ff572044bbe9d
K2976 <sanghikartik2976@gmail.com>
2026-05-22 18:56:39 +0530

    removed unwanted files

--- delete_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f Parkezy/Backend/FirebaseManager.swift
-rm -f Parkezy/Backend/AuthRepository.swift
-rm -f Parkezy/Backend/UserRepository.swift
-rm -f Parkezy/Backend/BookingRepository.swift
-rm -f Parkezy/Backend/PrivateListingRepository.swift
-rm -f Parkezy/Backend/CommercialFacilityRepository.swift
